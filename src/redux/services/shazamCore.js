622ceedf6c1d5d1f0e4ac0a70be35b87be4e7870
Yersultan <ersultansansyzbaj@gmail.com>
2023-05-29 22:04:03 +0600

    TopPlay

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -7,14 +7,16 @@ export const shazamCoreApi = createApi({
     prepareHeaders: (headers) => {
       headers.set(
         "X-RapidAPI-Key",
-        "67f436ea87msh63c3582447b58ebp153cecjsne2e8bd116dd0"
+        // "67f436ea87msh63c3582447b58ebp153cecjsne2e8bd116dd0"
+        "4f357e93a4msh206d7977649c51fp172ff7jsnc472bcb08bc8"
       );
-
       return headers;
     },
   }),
   endpoints: (builder) => ({
     getTopCharts: builder.query({ query: () => "/chart/tracks/top" }),
+    getArtist: builder.query({ query: () => "/chart/artists/top" }),
   }),
 });
 export const { useGetTopChartsQuery } = shazamCoreApi;
+export const { useGetArtistQuery } = shazamCoreApi;
